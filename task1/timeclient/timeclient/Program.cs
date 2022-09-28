6d1cae049f61ccce38a209bc74e7bb2954c34bb1
ArtemKhamzin <86554484+ArtemKhamzin@users.noreply.github.com>
2022-09-28 19:34:30 +0300

    task1

---
+++ task1/timeclient/timeclient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int port = 1303;
+
+Console.WriteLine("Введите IP адрес сервера");
+var address = Console.ReadLine() ?? throw new InvalidOperationException();
+var ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Connect(ipPoint);
+                
+var data = new byte[256];
+var builder = new StringBuilder();
+var bytes = socket.Receive(data, data.Length, 0);
+builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+Console.WriteLine("Сообщение сервера: " + builder);
+ 
+socket.Shutdown(SocketShutdown.Both);
+socket.Close();
---
+++ task1/timeserver/timeserver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int port = 1303;
+
+var ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+var listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+listenSocket.Bind(ipPoint);
+listenSocket.Listen();
+Console.WriteLine("Сервер запущен");
+ 
+while (true)
+{
+    var socket = listenSocket.Accept();
+    var data = Encoding.Unicode.GetBytes(DateTime.Now.ToString("dd.MM.yy HH:mm"));
+    socket.Send(data);
+    Console.WriteLine("Ответ отправлен");
+                    
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Close();
+}
